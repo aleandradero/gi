64fa999234e1fff3ee59df147b87aeeaf2d292f7
aleandradero <alexandra.randrade@senacsp.edu.br>
2025-02-12 10:20:32 -0300

    chore: create and apply ImageViewer component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,16 @@
 import {View, StyleSheet} from "react-native";
-import {Image} from "expo-image"
+import ImageViewer from "@/components/ImageViewer";
 
 const PlaceholderImage = require("@/assets/images/image.jpg")
 
-export default function Index() {
-
+export default function Index(){
   return (
     <View style={styles.container}>
-      <Image source={PlaceholderImage} style={styles.image}/>
+      <View style={styles.imageContainer}>
+        <ImageViewer imgSource={PlaceholderImage} />
+      </View>
     </View>
+    
   );
 }
 
@@ -21,10 +23,5 @@ const styles = StyleSheet.create({
   },
   imageContainer:{
     flex:1,
-  },
-  image:{
-    width:320,
-    height:440,
-    borderRadius: 18,
   },
 });
